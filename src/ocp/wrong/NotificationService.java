12a86babf66a12d6797f7b0d5211272397341095
Abhay Katiyar <abhaykatiyar@Abhays-MacBook-Pro.local>
2023-08-20 23:38:27 +0530

    Modified typos

--- src/ocp/wrong/NotificationService.java
+++ src/ocp/wrong/NotificationService.java
@@ -4,7 +4,7 @@ package ocp.wrong;
 /**
  * In the below class we have two medium of sending the notifications using mobile and email but let us say a client wants
  * to send notifications using WhatsApp also then we need to modify this class to send notification using Whatsapp.
- * If we add WhatsApp notifications then it is also possible that this service is not required by existing clients wo were
+ * If we add WhatsApp notifications then it is also possible that this service is not required by existing clients who were
  * using it.
  * By modifying the existing NotificationService it is breaking the Open/closed principle.
  * In the correct package we have added a separate interface for NotificationService and then using that for sending notifications
--- src/srp/srp.md
+++ src/srp/srp.md
@@ -1,4 +1,4 @@
-Single responsibility principle states that a class should do one thing and therefore it should a
+Single responsibility principle states that a class should do one thing, and therefore it should have only a
 single reason to change.
 
 In the correct package we have moved the all the extra functionality in BankService to different classes.
